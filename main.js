be9ccba3f28b59216afbfd04e55e60072aae9e25
pistoleirodaluz <129514045+pistoleirodaluz@users.noreply.github.com>
2024-06-19 07:48:35 -0300

    tje

--- main.js
+++ main.js
@@ -16,3 +16,8 @@ textos[i].classList.add("ativo");
 
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
