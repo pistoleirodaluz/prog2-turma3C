2d8be23b593f7d886fd6298593baa1e474bff981
pistoleirodaluz <129514045+pistoleirodaluz@users.noreply.github.com>
2024-06-19 07:59:17 -0300

    uy876

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ textos[i].classList.add("ativo");
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
